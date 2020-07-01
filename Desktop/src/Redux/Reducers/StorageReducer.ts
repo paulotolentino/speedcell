fde5a16c33b6588a410184a913e1acbeefe17de5
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2020-07-01 02:08:49 -0300

    Update storage reducer to accept isEditing and selectedProduct

--- Desktop/src/Redux/Reducers/StorageReducer.ts
+++ Desktop/src/Redux/Reducers/StorageReducer.ts
@@ -6,21 +6,51 @@ export interface ProductInterface {
   codigo_barras: string;
   preco_compra: number;
   preco_venda: number;
-  data_modificacao: Date;
+  data_modificacao: Date | null;
+  quantidade: number;
 }
 
 interface State {
-  data: Array<ProductInterface>;
+  data: {
+    storage: Array<ProductInterface>;
+    selectedProduct: ProductInterface;
+    isEditing: boolean;
+  };
 }
 
 export const initialState: State = {
-  data: [],
+  data: {
+    storage: [],
+    selectedProduct: {
+      codigo_barras: "",
+      data_modificacao: null,
+      id: 0,
+      nome: "",
+      preco_compra: 0,
+      preco_venda: 0,
+      quantidade: 0,
+    },
+    isEditing: false,
+  },
 };
 
 const StorageReducer = (state: State = initialState, action: any) => {
   switch (action.type) {
     case actionTypes.SET_STORAGE:
-      return { ...state, data: action.data } as State;
+      return {
+        ...state,
+        data: { ...initialState.data, storage: action.data },
+      } as State;
+    case actionTypes.CLEAN_STORAGE:
+      return {
+        ...state,
+        data: { ...initialState.data },
+      } as State;
+    case actionTypes.SET_PRODUCT:
+      return {
+        ...state,
+        data: { ...state.data, selectedProduct: action.data, isEditing: true },
+      } as State;
     default:
       return state;
   }
