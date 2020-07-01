07bf4728021e44dd1f77ba05c862a0b184ac6217
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2020-07-01 02:07:56 -0300

    Update actions

--- Desktop/src/Redux/Actions/index.ts
+++ Desktop/src/Redux/Actions/index.ts
@@ -9,3 +9,5 @@ export const SET_SERVICE_ORDERS = "SET_SERVICE_ORDERS";
 
 // Storage Actions
 export const SET_STORAGE = "SET_STORAGE";
+export const CLEAN_STORAGE = "CLEAN_STORAGE";
+export const SET_PRODUCT = "SET_PRODUCT";
