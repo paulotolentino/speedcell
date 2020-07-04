0a1cb1673b8f9fdd90064263c3551c14a8b6a5de
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2020-07-04 03:42:44 -0300

    Add Client reducer

--- Desktop/src/Redux/Reducers/ClientReducer.ts
+++ Desktop/src/Redux/Reducers/ClientReducer.ts
@@ -8,23 +8,57 @@ export interface ClientInterface {
   logradouro: string;
   numero: string;
   cidade: string;
-  uf: string;
+  estado: string;
   telefone: string;
   email: string;
+  data_criacao: Date | null;
 }
 
 interface State {
-  data: Array<ClientInterface>;
+  data: {
+    clients: Array<ClientInterface>;
+    selectedClient: ClientInterface;
+    isEditing: boolean;
+  };
 }
 
 export const initialState: State = {
-  data: [],
+  data: {
+    clients: [],
+    selectedClient: {
+      id: 0,
+      nome: "",
+      cpf: 0,
+      email: "",
+      telefone: "",
+      cep: 0,
+      logradouro: "",
+      numero: "",
+      cidade: "",
+      estado: "",
+      data_criacao: null,
+    },
+    isEditing: false,
+  },
 };
 
 const ClientsReducer = (state: State = initialState, action: any) => {
   switch (action.type) {
     case actionTypes.SET_CLIENTS:
-      return { ...state, data: action.data } as State;
+      return {
+        ...state,
+        data: { ...initialState.data, clients: action.data },
+      } as State;
+    case actionTypes.CLEAN_CLIENTS:
+      return {
+        ...state,
+        data: { ...initialState.data },
+      } as State;
+    case actionTypes.SET_CLIENT:
+      return {
+        ...state,
+        data: { ...state.data, selectedClient: action.data, isEditing: true },
+      } as State;
     default:
       return state;
   }
