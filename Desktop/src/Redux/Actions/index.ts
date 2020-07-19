124a73397c1ae561636f129a29b7c8489b00c85e
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2020-07-19 11:12:24 -0300

    CHange redux to save sales data

--- Desktop/src/Redux/Actions/index.ts
+++ Desktop/src/Redux/Actions/index.ts
@@ -5,6 +5,9 @@ export const SET_CLIENT = "SET_CLIENT";
 
 // Sales Actions
 export const SET_SALES = "SET_SALES";
+export const SET_CPF_SALE = "SET_CPF_SALE";
+export const SET_ID_SALE = "SET_ID_SALE";
+export const SET_SHOW_SALE = "SET_SHOW_SALE";
 
 // Service Orders Actions
 export const SET_SERVICE_ORDERS = "SET_SERVICE_ORDERS";
--- Desktop/src/Redux/Reducers/SaleReducer.ts
+++ Desktop/src/Redux/Reducers/SaleReducer.ts
@@ -1,65 +1,69 @@
 import * as actionTypes from "../Actions";
-
-export interface SaleInterface {
+export interface ProductProps {
   id: number;
-  num_compra: number;
+  numero_venda: number;
   id_cliente: number;
+  valor_desconto: number;
   data: Date;
+  nome: string;
+  cpf: number;
+  preco_venda: number;
+  cep: number;
   valor: number;
 }
 
 interface State {
-  data: Array<SaleInterface>;
+  data: {
+    sales: Array<ProductProps>;
+    idSelectedSale: number;
+    cpfSale: number;
+    isShowing: boolean;
+  };
 }
 
-const testData: State = {
-  data: [
-    {
-      id: 1,
-      num_compra: 112,
-      id_cliente: 3,
-      data: new Date(),
-      valor: 22.35,
-    },
-    {
-      id: 2,
-      num_compra: 113,
-      id_cliente: 2,
-      data: new Date(),
-      valor: 10,
-    },
-    {
-      id: 3,
-      num_compra: 114,
-      id_cliente: 1,
-      data: new Date(),
-      valor: 11.99,
-    },
-    {
-      id: 4,
-      num_compra: 115,
-      id_cliente: 3,
-      data: new Date(),
-      valor: 80,
-    },
-    {
-      id: 5,
-      num_compra: 116,
-      id_cliente: 1,
-      data: new Date(),
-      valor: 50,
-    },
-  ],
-};
-
 export const initialState: State = {
-  data: [],
+  data: {
+    sales: [],
+    idSelectedSale: 0,
+    cpfSale: 0,
+    isShowing: false,
+  },
 };
 
-const SalesReducer = (state: State = testData, action: any) => {
+const SalesReducer = (state: State = initialState, action: any) => {
   switch (action.type) {
     case actionTypes.SET_SALES:
-      return { ...state, data: action.data } as State;
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          sales: action.data,
+        },
+      } as State;
+    case actionTypes.SET_CPF_SALE:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          cpfSale: action.data,
+        },
+      } as State;
+    case actionTypes.SET_ID_SALE:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          idSelectedSale: action.data,
+        },
+      } as State;
+    case actionTypes.SET_SHOW_SALE:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          isShowing: action.data,
+        },
+      } as State;
     default:
       return state;
   }
