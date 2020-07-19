f10871ec2216a0104aa94f8631a4198ae388de5b
Paulo Tolentino <paulo.sergio1001@hotmail.com>
2020-07-19 11:03:14 -0300

    Add new routes

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -10,10 +10,15 @@ const clientsController = new ClientsController();
 
 routes.get("/vendas", salesController.index);
 routes.get("/vendas/:id", salesController.show);
+routes.get("/getvendas/numero", salesController.getNumMax);
 routes.post("/vendas", salesController.create);
+// routes.put("/vendas/:id", salesController.change);
+// routes.delete("/vendas/", salesController.delete);
+// routes.delete("/vendas/:id", salesController.deleteOne);
 
 routes.get("/produtos", productsController.index);
 routes.get("/produtos/:id", productsController.show);
+routes.get("/produtos/barcode/:codBarras", productsController.searchByBarCode);
 routes.post("/produtos", productsController.create);
 routes.put("/produtos/:id", productsController.change);
 routes.delete("/produtos/", productsController.delete);
@@ -21,6 +26,7 @@ routes.delete("/produtos/:id", productsController.deleteOne);
 
 routes.get("/clientes", clientsController.index);
 routes.get("/clientes/:id", clientsController.show);
+routes.get("/clientes/cpf/:cpf", clientsController.searchByCPF);
 routes.post("/clientes", clientsController.create);
 routes.put("/clientes/:id", clientsController.change);
 routes.delete("/clientes/", clientsController.delete);
